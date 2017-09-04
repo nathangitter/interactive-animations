2780751c9e239a9f25f449a395089b25461115bb
Nathan Gitter <nathan@nathangitter.com>
2017-09-04 14:48:51 -0500

    initial commit

---
+++ InteractiveAnimations/InteractiveAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveAnimations
+//
+//  Created by Nathan Gitter on 9/4/17.
+//  Copyright © 2017 Nathan Gitter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
